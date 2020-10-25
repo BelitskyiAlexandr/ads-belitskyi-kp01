9725f6c5423bbfa0752edaf0c49dcacfa48cad84
BelitskyiAlexandr <vilisapett@gmail.com>
2020-10-25 16:23:05 +0200

    lab 1 code

---
+++ lab_1/part_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+
+namespace part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter x: ");
+            int x = int.Parse(ReadLine());
+            WriteLine("Enter y: ");
+            int y = int.Parse(ReadLine());
+            WriteLine("Enter z: ");
+            int z = int.Parse(ReadLine());
+
+            double a = Pow(x, z) + Log10(Abs(5 * x + y));
+            double b = 1 / (a * a) + (Sqrt(x - y)) / z;
+
+            if ((5 * x + y == 0) || (z == 0) || (a == 0) || ((x < y) && (0 <= x)))
+            {
+                WriteLine("Your numbers are wrong. Please, change variable value.");
+            }
+            else
+            {
+
+
+                WriteLine("Number a : {0}", a);
+                WriteLine("Number b : {0}", b);
+            }
+        }
+    }
+}
---
+++ lab_1/part_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+
+
+namespace part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter n: ");
+            int n = int.Parse(ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (IsItTrue(i))
+                {
+                    Write("Number {0} is ending of his square: {1}", i, i*i);
+                    WriteLine();
+                }
+
+            }
+        }
+
+
+
+        static int amount(int x)
+        {
+            int am = 0;
+            while (x > 0)
+            {
+                am += 1;
+                x = x / 10;
+            }
+            return am;
+        }
+
+
+        static int nums(int x)
+        {
+            int nu = 1;
+            for (int i = 1; i <= x; i += 1)
+            {
+                nu = nu * 10;
+            }
+            return nu;
+        }
+
+
+
+        static bool IsItTrue(int x)
+        {
+            if (x == (x * x) % nums(amount(x)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
